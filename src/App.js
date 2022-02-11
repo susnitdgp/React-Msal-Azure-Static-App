86a3e79945047c6c718845aa949e8fff947e1578
susnitdgp <susnitdgp@gmail.com>
2022-02-11 21:36:09 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -39,19 +39,17 @@ function App() {
     <Layout className="mainLayout">
     <Header>
         <AppHeader/>
-      </Header>
+    </Header>
 
-      <Content style={{ padding: "0 50px" }}>
-        <Layout
-          className="site-layout-background"
-          style={{ padding: "24px 0" }}
-        >
-          <Sider className="site-layout-background" width={200}>
+      <Content >
+        <Layout>
+          
+          <Sider width={200} style={{ margin:"100px", }}>
             <Menu
               mode="inline"
               defaultSelectedKeys={["1"]}
               defaultOpenKeys={["sub1"]}
-              style={{ height: "100%" }}
+              style={{ height: "100%"}}
             >
               <SubMenu key="sub1" icon={<UserOutlined />} title="Admin Menu">
                 <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
