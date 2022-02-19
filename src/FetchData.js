292f0f9399c899201ab33dbe8b6a0851e6dc8b81
Susanta Goswami <susnitdgp@gmail.com>
2022-02-19 21:35:20 +0530

    Progres bAr

--- src/FetchData.js
+++ src/FetchData.js
@@ -1,18 +1,29 @@
 import React , { useState,useEffect } from "react";
-import { Badge} from 'antd';
+import { Badge, message} from 'antd';
 import axios from 'axios';
+import { loadProgressBar } from 'axios-progress-bar';
+import 'axios-progress-bar/dist/nprogress.css';
+
+const key = 'updatable';
 
 export const FetchData=()=>{
+
+    loadProgressBar();
 
     const [total, setTotal] = useState(120);
 
     useEffect(() => {
+        message.loading({ content: 'Loading...', key });
         // GET request using axios inside useEffect React hook
         axios.get('https://api.npms.io/v2/search?q=react')
             .then(response => {
                 console.log(response.data);
                 setTotal(response.data.total);
-            });
+                message.success({ content: 'Loaded!', key, duration: 2 });
+            })
+            .catch(err => {
+                console.log("error: ", err.message);
+              });
     
         // empty dependency array means this effect will only run once (like componentDidMount in classes)
     }, []);
